7476dd99db37fc714f1205e3b9d78baee79c9b4e
Mr.Green <roopxh@gmail.com>
2023-12-05 10:57:41 +0600

    Create a state file. Initial setup.

--- src/index.js
+++ src/index.js
@@ -3,36 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { state } from './redux/state';
 
 // мои игры
-
-export let dialogsData = [
-  { id: '1', name: 'Никита' },
-  { id: '2', name: 'Стас' },
-  { id: '3', name: 'Гена' },
-  { id: '4', name: 'Турбо' },
-  { id: '5', name: 'Дюша Метёлкин' }
-]
-
-export let msgData = [
-  { id: '1', text: 'ipsum dolor sit' },
-  { id: '2', text: 'Lorem ipsum dolor bro' },
-  { id: '3', text: 'Lorem ipsum sit bro' },
-  { id: '4', text: 'Lorem dolor sit bro' },
-  { id: '5', text: 'ipsum dolor sit bro Метёлкин' }
-]
-
-export let posts = [
-  { id: 1, msg: 'Hi how are u?', likesCount: 21 },
-  { id: 2, msg: 'Hi Amogus!', likesCount: 1 },
-  { id: 3, msg: 'Go walk?', likesCount: 21 },
-  { id: 4, msg: 'No, I`m domosed', likesCount: 2 },
-]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App posts={posts} msg={msgData} dialog={dialogsData} />
+    <App posts={state.posts} msg={state.msg} dialog={state.dialogs} />
   </React.StrictMode>
 );
 
