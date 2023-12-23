ff2ca033c21ce61be60622c9ade3f955b875e531
Mr.Green <roopxh@gmail.com>
2023-12-23 12:45:20 +0600

    Add getUser API.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,25 +1,33 @@
 import React from "react";
 import icon1 from '../../redux/icons/1_icon.png'
-import icon2 from '../../redux/icons/1_icon.png'
-import icon3 from '../../redux/icons/1_icon.png'
+import icon2 from '../../redux/icons/2_icon.png'
+import icon3 from '../../redux/icons/3_icon.png'
+import axios from 'axios'
+
+// [{ id: 1, img: icon1, followed: false, fullName: 'Denis', status: 'I`m a boss', location: { city: 'Majong', country: 'China' } },
+// { id: 2, img: icon2, followed: false, fullName: 'Pasha', status: 'I`m a lucky', location: { city: 'Helsinki', country: 'Finland' } },
+// { id: 3, img: icon3, followed: true, fullName: 'Petr', status: 'I`m a joker', location: { city: 'Goodlend', country: 'USA' } },]
 
 export let Users = (props) => {
    // console.log('Users props: ', props)
    if (props.users.length === 0) {
-      props.setUsers([
-         { id: 1, img: icon1, followed: false, fullName: 'Denis', status: 'I`m a boss', location: { city: 'Majong', country: 'China' } },
-         { id: 2, img: icon2, followed: false, fullName: 'Pasha', status: 'I`m a lucky', location: { city: 'Helsinki', country: 'Finland' } },
-         { id: 3, img: icon3, followed: true, fullName: 'Petr', status: 'I`m a joker', location: { city: 'Goodlend', country: 'USA' } },
-      ])
+      axios.get('https://social-network.samuraijs.com/api/1.0/users').then(Response => {
+         // debugger
+         props.setUsers(
+            Response.data.items
+         )
+      })
    }
+
    return <div> {
 
       props.users.map((u, i) => <div key={u.id + i}>
          <span>
             <div>
-               <img src={u.img} alt="" />
+               <img src={u.photos.small !== null
+                  ? icon1
+                  : icon2} alt="" />
             </div>
-
             <div>
                {u.followed
                   ? <button onClick={() => { props.unfollow(u.id) }}>unfollow</button>
@@ -27,18 +35,16 @@ export let Users = (props) => {
                }
             </div>
          </span>
-
          <span>
             <span>
-               <div>{u.fullname}</div>
+               <div>{u.name}</div>
                <div>{u.status}</div>
             </span>
             <span>
-               <div>{u.location.city}</div>
-               <div>{u.location.country}</div>
+               <div>{'u.location.city'}</div>
+               <div>{'u.location.country'}</div>
             </span>
          </span>
-
       </div>
       )
    }
